3171e34e1bc3849d8f63f93c47b612907073959c
Ihor Debelyi <ihor.debelyi@gmail.com>
2023-11-14 22:39:09 +0100

    Mapty project init and added markers handling

---
+++ Mapty/script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 14);
+
+      L.tileLayer('https://tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (mapEvent) {
+        const {
+          latlng: { lat: markerLatitude, lng: markerLongitude },
+        } = mapEvent;
+        //or
+        // const {lat: markerLatitude, lng: markerLongitude} = mapEvent.latlng;
+        const markerCoords = [markerLatitude, markerLongitude];
+        console.log(markerLatitude, markerLongitude);
+
+        L.marker(markerCoords)
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
